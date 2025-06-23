80e2f689022bda798c1662fad965e5860c4c9f38
Giulia-Soares <giulinha4@gmail.com>
2025-06-23 20:07:48 -0300

    Finalizado

--- ticket_bus.py
+++ ticket_bus.py
@@ -1,74 +1,111 @@
 import os
 import datetime 
 
+# 0 = menu
+# 1 = menu compra
+# 2 = Alterar quantidade de assentos
+# 3 = Monetario
+
 continua = True
 assentos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
-mensagem1 = 'Opção Inválida. Tente Novamente'
-
-altura = 24
-# Para limpar o Terminal
-# os.system('clear')
-
+mensagem = ""
+screen_flag = 0
 qtde_assentos = 52
 
+def partir_onibus():
+    assentos = []
+    for i in range(qtde_assentos):
+        assentos.append(i + 1)
+
+    return assentos
+
 def menu():
+
+    global mensagem, screen_flag, assentos
+
     agora = datetime.datetime.now()
     texto = agora.strftime("                                 Ticket System                         %H:%M:%S ")
-    print(texto)
-
-    print("\n\n\n1 - Comprar ticket\n\n2 - Partir onibus\n\n3 - Alterar quantidade de assentos\n\n4 - Monetario\n\nX - Fechar programa\n\n")
-    resp = input("\n\n\n\n\n\n\n\nDigite uma dessas opções: ")
     
-    return resp
+    os.system('clear')
 
+    print(texto)
+    print("\n\n\n1 - Comprar ticket")
+    print("\n2 - Partir onibus")
+    print("\n3 - Alterar quantidade de assentos")
+    print("\n4 - Monetario\n")
+    print("X - Fechar programa\n")
+
+    print(f"\n\n\n\n\n\n\n{mensagem.upper()}")
+
+    option = input("Digite uma dessas opções: ")
+
+    if option.lower() == 'x':
+        os.system('clear')
+        exit()
+    else:
+        os.system('clear')
+        mensagem = 'opcao invalida'
+
+    if option.isnumeric():
+        if option == "1":
+            screen_flag = 1
+            return
+        elif option == "2":
+            assentos = partir_onibus()
+            mensagem = 'Onibus partiu! Assentos para a proxima viagem ja disponiveis!'
+        elif option == "3":
+            mensagem = 'Tela para alterar quantidade de assentos nao implementada'
+        elif option == '4':
+            mensagem = 'Tela do Monetario nao implementada'
+        else:
+            mensagem = 'Tela inexistente'
 
 def menu_compra():
+
+    global mensagem, screen_flag, assentos
+
     agora = datetime.datetime.now()
     texto = agora.strftime("                                 Ticket System                         %H:%M:%S ")
-    print(texto)
-    
-    print('\n\nEscolha seu assento\n\n')
-    print(assentos)
-    resp2 = input("\n\n\n\n\n\n\n\n\nEscolha uma opção: ")
 
-    if resp2 == 'x' or resp2 == 'X':
-        exit()
-    elif resp2 == 'b' or resp2 == 'B':
-        print('Voltar para o menu anterior')
-    elif resp2.isnumeric and int(resp2) > 0 and int(resp2) <= qtde_assentos:
-        escolha_assento = int(resp2)
-        indice = int(resp2) - 1
-        if escolha_assento < 1 or escolha_assento > qtde_assentos:
-            print('Assento nao encontrado. Tente Novamente')
-            escolha_assento = input("\n\nDigite o assento escolhido: ")
+    os.system('clear')
+
+    print(texto)
+    print('\n\nEscolha seu assento\n')
+    print(assentos)
+
+    print(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n{mensagem.upper()}")
+
+    option = input("Escolha uma opção: ")
+
+    if option.lower() == 'x':
+        os.system('clear')
+        exit() 
+    elif option.lower() == 'b':
+        screen_flag = 0
+        return
+
+    if option.isnumeric():  
+        option = int(option)
+        if option > 0 and option <= qtde_assentos:
+            i = option -1
+            assentos[i] = 'X'
+            print(assentos)
+            mensagem = f'assento de numero {option} escolhido'
         else:
-            for escolha_assento in assentos:
-                assentos[indice] = "X"
-                print(assentos)
-                break
+            mensagem = 'assento inexistente'
     else:
-        print('Assento nao encontrado. Tente novamente')
-    
-        
-#def partir_onibus():
+        mensagem = 'opcao invalida'
   
 #main
 while continua:
-     
-    resp = menu()
-    os.system('clear')
-    if resp != "1" and resp != "2" and resp != "3" and resp != "4" and resp != "x" and resp != "X":
-        os.system('clear')
-        print(mensagem1)
-    elif resp == "1":
-        menu_compra()
-        print('Compra do ticket realizada')
-    elif resp == "2":
-        print('.')
-        #partir_onibus()
-    elif resp == "3":
-        print("Alterar quantidade de assentos")
-    elif resp == "4":
-        print("Monetario")
-    else:
-        continua = False
+
+        if screen_flag == 0:
+            menu()
+        elif screen_flag == 1:
+            menu_compra()
+        elif screen_flag == 2:
+            mensagem = 'Tela Indisponivel'
+        elif screen_flag == 3:
+            mensagem = 'Tela indisponivel'
+        else:
+            mensagem = 'Tela inexistente'
